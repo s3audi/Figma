e1a94cb33dea59c27f15c98e50a0b969b7ef7504
s3audi <s3audi@hotmail.com>
2020-10-16 11:12:38 +0300

    Update gg.bat

--- gg.bat
+++ gg.bat
@@ -15,8 +15,9 @@ git commit -m "$1"
 git push -u origin master
 }
 
+https://dynalist.io/d/ZA5D4lt6LFmP2Paj3n_je4OD#z=T5U9KpyU964rQEaRoTNW79hf
 
-# 
+# https://dynalist.io/d/ZA5D4lt6LFmP2Paj3n_je4OD#z=T5U9KpyU964rQEaRoTNW79hf
 # 
 # 
 # 
